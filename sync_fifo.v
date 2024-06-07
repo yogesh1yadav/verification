d5074def7ac48231571855b18d1700f26b618c48
yogesh1yadav <yadav.yogesh862@gmail.com>
2024-06-07 06:07:23 +0530

    sync_fifo.v

--- sync_fifo.v
+++ sync_fifo.v
@@ -1,110 +1,108 @@
 `timescale 1ns / 1ps
 
-module tb_sync_fifo;
-parameter depth = 8;
-parameter width = 8;
-parameter ptr_width = $clog2(depth);
-    
-reg     clk_i, rst_i, wr_en_i, rd_en_i;
-reg     [width-1:0] wr_data_i;
-wire    [width-1:0] rd_data_o;
-wire    full_o, empty_o, wr_error_o, rd_error_o;
-    
+module sync_fifo (clk_i, rst_i, wr_en_i, rd_en_i, wr_data_i, rd_data_o, full_o, empty_o, wr_error_o, rd_error_o);
+parameter depth=8, width=8;                   
+parameter ptr_width = $clog2(depth);        
+
+input clk_i, rst_i, wr_en_i, rd_en_i;               //wr_en_i       : write enable input               rd_en_i      : read enable input
+input [width-1:0] wr_data_i;                        //wr_data_i     : write data input              
+output reg [width-1:0] rd_data_o;                   //rd_data_o     : read data output
+output reg full_o, empty_o, wr_error_o, rd_error_o;
+
+reg [ptr_width-1:0] wr_ptr, rd_ptr;                 //wr_ptr        : write pointer                    rd_ptr       : read pointer
+reg wr_toggle_f, rd_toggle_f;                       //wr_toggle_f   : write toggle flag                rd_toggle_f  : read toggle flag
+
+reg [width-1:0] mem [depth-1:0];                    //declare memory
+
 integer i;
-    
-// Instantiate the FIFO
-sync_fifo dut(clk_i, rst_i, wr_en_i, rd_en_i, wr_data_i, rd_data_o, full_o, empty_o, wr_error_o, rd_error_o);
-    
-// Generate clock signal
-initial begin
-    clk_i=0;
-    forever #10 clk_i=~clk_i;
-end    
-    
-// Test procedure
-initial begin
- // Initialize signals
-    rst_i = 1;
-    wr_en_i = 0;
-    rd_en_i = 0;
-    wr_data_i = 0;
-    
-    #20;
-    rst_i = 0;
-    
-    
- // Write to FIFO till it gets full
-    for (i = 0; i < depth; i = i + 1) begin
-        @(posedge clk_i);
-        begin
-            wr_data_i = $random;                        // writing Random data
-            wr_en_i = 1;
-        end            
-    end
-    
-    @(posedge clk_i);    
-    wr_en_i = 0;      
-    
-    
- // Attempt to write when FIFO is full
-    @(posedge clk_i);
-    begin
-        wr_data_i = $random;
-        wr_en_i = 1;
-    end
-    
-    @(posedge clk_i);       
-    wr_en_i = 0;
-    
-    
- // Read from FIFO 5 stored data 
-    for (i = 0; i < 5; i = i + 1) begin
-        @(posedge clk_i);
-        rd_en_i = 1;
-    end
-    
-    @(posedge clk_i);
-    rd_en_i = 0;
-    
-    
- // Attempt to write again after some reading to check full and empty conditions  
-    for (i = 0; i < 4; i = i + 1) begin
-        @(posedge clk_i);
-        begin
-            wr_data_i = $random;  
-            wr_en_i = 1;
-        end
-    end    
+
+always @ (posedge clk_i) begin      
+    if (rst_i) begin                                // reset signal enable
+        rd_data_o=0;                                // all reg variable assign to reset value 0
+        full_o=0;
+        empty_o=0;
+        wr_error_o=0; 
+        rd_error_o=0;
+        wr_ptr=0;
+        rd_ptr=0;
+        wr_toggle_f=0; 
+        rd_toggle_f=0;
         
-    @(posedge clk_i);
-    wr_en_i = 0;
-      
-            
- // Read from FIFO again         
-    for (i = 0; i < depth; i = i + 1) begin
-        @(posedge clk_i);
-            rd_en_i = 1;
-    end 
-                
-    @(posedge clk_i);
-    rd_en_i = 0;
+        for (i=0;i<depth;i=i+1) begin
+            mem[i]=0;                               // reset full memory  
+        end
+    end
     
     
- // Attempt to read when FIFO is empty
-    @(posedge clk_i);
-    rd_en_i = 1;
-    
-    @(posedge clk_i);
-    rd_en_i = 0;
-    
- // Test reset
-    @(posedge clk_i);
-    rst_i = 1;
-    
-    #20;
-    rst_i = 0;
-    
-    #30;
-    $finish;
+// WRITE OPERATION  
+    else begin                                      // reset signal disable
+        if (wr_en_i==1) begin                        
+            if(full_o==1) begin                     // checking for full condition to perform write operation
+                wr_error_o=1;
+            end
+            else begin
+                wr_error_o=0;
+                mem[wr_ptr]=wr_data_i;              // store or write data into FIFO
+                if (wr_ptr==depth-1) begin
+                    wr_ptr=0;
+                    wr_toggle_f=~wr_toggle_f;
+                end
+                else begin
+                    wr_ptr=wr_ptr+1;
+                end
+            end
+        end
+        else begin 
+            wr_error_o=0;                           // Clear write error if not writing
+        end
+        
+        
+// READ OPERATION     
+        if (rd_en_i==1) begin                                         
+            if(empty_o==1) begin                    // checking for empty condition to perform read operation
+                rd_error_o=1;
+            end
+            else begin
+                rd_error_o=0;
+                rd_data_o=mem[rd_ptr];
+                if (rd_ptr==depth-1) begin          // collect or read data from FIFO
+                    rd_ptr=0;
+                    rd_toggle_f=~rd_toggle_f;
+                end
+                else begin
+                    rd_ptr=rd_ptr+1;
+                end
+            end    
+        end
+        else begin
+            rd_error_o = 0;                         // Clear read error if not reading
+        end  
+    end
+end
+   
+   
+//FULL OR EMPTY SIGNAL GENERATION    
+always@(*) begin                                    
+    empty_o=0;
+    full_o=0;
+    if (wr_ptr==rd_ptr) begin
+        if (wr_toggle_f==rd_toggle_f) begin
+            empty_o=1;
+        end
+        if (wr_toggle_f!=rd_toggle_f) begin
+            full_o=1;
+        end
+    end
 end
 endmodule
+
+
+
+                
+                
+     
+    
+
+
+
+
